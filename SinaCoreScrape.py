d27b3da06ae15484a96e569d4d2dd136973a0522
Yishiyork <3051176425@qq.com>
2024-12-29 12:16:21 +0800

    ✨ feat: 修改了指定报告类型的部分
    完成了上次的 Todo 任务，修改了指定报告类型的部分，新增指定报告类型的可选项，需要覆盖网站提供的全部类型，并提供多选、全选的便捷操作，而非使用循环。

--- SinaCoreScrape.py
+++ SinaCoreScrape.py
@@ -12,16 +12,6 @@ HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0',
 }
-TYPES = {
-    "个股": ["公司", "创业板"],
-    "行业": ["行业"],
-    "策略": ["策略"],
-    "宏观": ["宏观"],
-    "基金": ["基金"],
-    "债券": ["债券"],
-    "晨报": ["晨报"],
-}
-
 
 df = pd.read_excel(r"scrape-sina-broker-report\basicInfo.xlsx", header=None, names=[
     "stock_code", "company_name"])
@@ -52,7 +42,14 @@ def unpack_and_standarise_response(parsed_html):
     # 处理研究员
     file_researcher = parsed_html.xpath(
         '//div[@class="main"]/table/tr/td[6]/div/span/text()')
-    file_info = [file_url, file_title, file_type,
+    file_id = []
+    for ty, ti in list(zip(*(file_type, file_title))):
+        if ty in ["公司", "创业板"]:
+            file_id.append(find_stock_code(ti))
+        else:
+            file_id.append(ty)
+
+    file_info = [file_id, file_url, file_title, file_type,
                  file_broker, file_researcher]
     file_info = list(zip(*file_info))
     return file_info
@@ -80,62 +77,75 @@ def find_stock_code(input_string):
 
 
 class DateProcesser:
-    def __init__(self, reportDate, saving_path, report_types, proxies={}):
+    def __init__(self, reportDate, saving_path, customerized_type=[""], proxies={}):
         self.reportDate = reportDate
         self.csv_index = ["股票代码", "券商简称", "发布日期",
                           "研报标题", "报告链接", "研报文本", "研究员"]
-        self.saving_path = f"{saving_path}\分析师{report_types}报告"
+        self.saving_path = f"{saving_path}"
+        self.customerized_type = customerized_type
         self.proxies = proxies
-        self.report_types = TYPES[report_types]
 
     def process_page_for_downloads(self):
         """处理指定页码的公告信息并下载相关文件"""
-        # 持久化存储
-        saving_file = f"{self.saving_path}\{self.reportDate[:7]}.csv"
-        if not os.path.exists(saving_file):
-            df = pd.DataFrame(columns=self.csv_index)
-            df.to_csv(saving_file, index=False)
-        # 以文件链接作为主键，防止重复下载
-        df = pd.read_csv(saving_file, encoding='utf-8-sig',
-                         encoding_errors="ignore", dtype=str)
-        urls = [str(row["报告链接"])
-                for index, row in df.iterrows()]
         first_url = f'https://stock.finance.sina.com.cn/stock/go.php/vReport_List/kind/search/index.phtml?t1=6&symbol=&p=1&pubdate={self.reportDate}'
         parsed_html_first = scrape_page(first_url, HEADERS, self.proxies)
         total_pages = parsed_html_first.xpath(
-            '//a[text()="最末页"]/@onclick')[0].split("set_page_num(\'")[1].split("\')")[0]
+            '//a[text()="最末页"]/@onclick')
+        if not total_pages:
+            tqdm.write(f"{self.reportDate} 为空，跳过")
+            return
+        total_pages = total_pages[0].split(
+            "set_page_num(\'")[1].split("\')")[0]
         total_pages = int(total_pages)
         for pageNum in tqdm(range(total_pages), desc=f"{self.reportDate}"):
             URL = f'https://stock.finance.sina.com.cn/stock/go.php/vReport_List/kind/search/index.phtml?t1=6&symbol=&p={pageNum + 1}&pubdate={self.reportDate}'
             parsed_html = scrape_page(URL, HEADERS, self.proxies)
             file_info = unpack_and_standarise_response(parsed_html)
             for files in file_info:
-                self.download_file(files, urls)
+                self.download_file(files)
 
-    def download_file(self, files, urls):
+    def download_file(self, files):
         """分块下载文件"""
-        (url, title, type, broker, researcher) = files
-        # 跳过不是个股研究的报告
-        if type not in self.report_types:
-            # tqdm.write(f"\t不是{self.report_types}文件：{type}\t{title}")
-            return
-        # 从标题中获取代码、简称和文章题目
-        if type in ["公司", "创业板"]:
-            ids = find_stock_code(title)
+        (file_id, url, title, type, broker, researcher) = files
+        file_short_name = f"{file_id}_{broker}_{self.reportDate}"
+
+        # 跳过未指定的类型
+        if self.customerized_type == [""]:
+            self.customerized_type = ["行业", "公司",
+                                      "创业板", "债券", "策略", "晨报", "基金", "宏观"]
+        elif self.customerized_type == ["个股"]:
+            self.customerized_type = ["公司", "创业板"]
         else:
-            ids = type
-        file_short_name = f"{ids}_{broker}_{self.reportDate}"
-        # 组成文件在表中的列表
-        csv_info_list = [ids, broker, self.reportDate,
-                         title, url, [], researcher]
-        # 如果主键已经存在在文件中，则跳过
+            self.customerized_type = self.customerized_type
+        if type not in self.customerized_type:
+            # tqdm.write(f"\t{file_short_name}：不属于指定的报告类型")
+            return
+
+        # 指定下载文件夹
+        if type in ["公司", "创业板"]:
+            downloading_type = "个股"
+        else:
+            downloading_type = type
+
+        # 以文件链接作为主键，防止重复下载
+        saving_file = f"{self.saving_path}\分析师{downloading_type}报告\{self.reportDate[:7]}.csv"
+        if not os.path.exists(saving_file):
+            df = pd.DataFrame(columns=self.csv_index)
+            df.to_csv(saving_file, index=False)
+        df = pd.read_csv(saving_file, encoding='utf-8-sig',
+                         encoding_errors="ignore", dtype=str)
+        urls = [str(row["报告链接"])
+                for index, row in df.iterrows()]
         if url in urls:
             tqdm.write(f"\t{file_short_name}：已存在，跳过")
             return
+
+        # 组成文件在表中的列表
+        csv_info_list = [file_id, broker, self.reportDate,
+                         title, url, [], researcher]
         # 获取文件内容
         csv_info_list[5] = get_file_content(url, self.proxies)
         # 追加文件
-        saving_file = f"{self.saving_path}\{self.reportDate[:7]}.csv"
         df = pd.DataFrame([csv_info_list], columns=self.csv_index)
         df.to_csv(saving_file, mode='a', header=False, index=False)
         tqdm.write(f"{file_short_name}：已保存")
@@ -148,7 +158,7 @@ def retry_on_failure(func):
         result = func()
         return result
     except Exception as e:
-        tqdm.write(f'Error, {e}')
+        tqdm.write(f'Error, 重试中')
         time.sleep(pause_time)
         return retry_on_failure(func)
 
@@ -196,11 +206,10 @@ def get_file_content(url, proxies):
 # """此处为需要修改的代码"""
 # start_date = "2004-06-01"  # "2004-06-01"
 # end_date = None  # None # "2023-09-09"
-# saving_path = r"N:\Source_for_sale\分析师研报"
-# report_type = "策略"
+# saving_path = r""
 
 # ######################################################
 # if __name__ == "__main__":
 #     for Times in create_date_intervals(start_date, end_date)[::-1]:
-#         if DateProcesser(Times, saving_path, report_type, proxies={}).process_page_for_downloads() == False:
+#         if DateProcesser(Times, saving_path, proxies={}).process_page_for_downloads() == False:
 #             break
